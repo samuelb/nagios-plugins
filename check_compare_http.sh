369416bc56ac3f5a1b6b86fdeb285e7780678ed0
Samuel Barabas <samuel@owee.de>
2016-04-07 11:26:58 +0200

    Added check_compare_http.sh - check if two or more webserver delivers the same http content

---
+++ check_compare_http.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+STATE_DEPENDENT=4
+CRITICAL=false
+URLS=""
+
+
+CMDMD5=$(which md5sum)
+if [ -z $CMDMD5 ]; then
+    CMDMD5=$(which md5)
+fi
+if [ -z $CMDMD5 ]; then
+    echo "Could not find md5/md5sum command" >&2
+    exit $STATE_UNKNOWN
+fi
+
+
+print_help() {
+cat << HELP
+Compares HTTP body the content of multiple URLs and alarms if it differs.
+
+Usage:
+$(basename $0) [-h] [-c] URL-1 URL-2 [... URL-n]
+
+Options:
+-h
+    Show this help text.
+
+-c
+    Raise a critical result instead of warning.
+
+URL
+    A http/https URL to compare content of.
+
+HELP
+}
+
+ARGS="$*"
+
+for ARG in $ARGS; do
+    case $ARG in
+        "-c")
+            CRITICAL=true
+            ;;
+        "-h"|"--help")
+            print_help
+            exit $STATE_UNKOWN
+            ;;
+        *)
+            URLS="$URLS $ARG"
+    esac
+done
+
+if [ $(wc -w <<< "$URLS") -lt 2 ]; then
+    echo "You need to specify at least two URLS" >&2
+    exit $STATE_UNKNOWN
+fi
+
+OTHERMD5=""
+DIFFFOUND=false
+
+for URL in $URLS; do
+    MD5=$(curl -s $URL | $CMDMD5)
+    echo $URL : $MD5
+    if [ -z $OTHERMD5 ]; then
+        OTHERMD5=$MD5
+    else
+        if [ $MD5 != $OTHERMD5 ]; then
+            DIFFFOUND=true
+        fi
+    fi
+done
+
+if $DIFFFOUND; then
+    if $CRITICAL; then
+        echo "CRITICAL - The URLs deliver different content"
+        exit $STATE_CRITICAL
+    else
+        echo "WARNING - The URLs deliver different content"
+        exit $STATE_WARNING
+    fi
+fi
+
+echo "OK - All URLs deliver the same content"
+exit $STATE_OK
+
